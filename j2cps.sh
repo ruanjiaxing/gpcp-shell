880b19319aad7156e34d494b477fb952aff65b70
ruanjiaxing <62635950+ruanjiaxing@users.noreply.github.com>
2020-05-20 10:00:48 +0700

    Add files via upload

--- j2cps.sh
+++ j2cps.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -DCPSYM=.:$JRoot/symfiles:$JRoot/symfiles/JvmSystem -jar $JRoot/jars/J2cps.jar $1 $2 $3 $4 $5 $6 $7 $8 $9
+java -DCPSYM=.:$JRoot/symfiles:$JRoot/symfiles/JvmSystem -jar $JRoot/jars/j2cps.jar $1 $2 $3 $4 $5 $6 $7 $8 $9
